73b554ef9622519d11bd3d7d911fc2b36ef315d5
Puyu <49837577+Z-Puyu@users.noreply.github.com>
2025-10-23 00:39:23 +0800

    Rename SalaryTest::rand to RAND

--- src/test/java/seedu/address/model/person/SalaryTest.java
+++ src/test/java/seedu/address/model/person/SalaryTest.java
@@ -7,35 +7,35 @@ import java.util.Random;
 import org.junit.jupiter.api.Test;
 
 class SalaryTest {
-    private static final Random rand = new Random();
+    private static final Random RAND = new Random();
 
     @Test
     void toDouble() {
-        double value = rand.nextDouble(100000);
+        double value = RAND.nextDouble(100000);
         assertEquals(new Salary(value).toDouble(), Double.parseDouble(String.format("%.2f", value)));
     }
 
     @Test
     void inCents() {
-        double value = rand.nextDouble(100000);
+        double value = RAND.nextDouble(100000);
         assertEquals(new Salary(value).inCents(), (int) Math.round(value * 100));
     }
 
     @Test
     void testToString() {
-        double value = rand.nextDouble(100000);
+        double value = RAND.nextDouble(100000);
         assertEquals(new Salary(value).toString(), String.format("$%.2f / month", value));
     }
 
     @Test
     void integerPart() {
-        double value = rand.nextDouble(100000);
+        double value = RAND.nextDouble(100000);
         assertEquals(new Salary(value).integerPart(), (int) value);
     }
 
     @Test
     void decimalPart() {
-        double value = rand.nextDouble(100000);
+        double value = RAND.nextDouble(100000);
         assertEquals(new Salary(value).decimalPart(), (int) Math.round((value - (int) value) * 100));
     }
 }
