e7ebaea15482822c7d25f19a555856db5594a7c7
Caleb Goh <calebgoh05@gmail.com>
2025-10-29 23:40:46 +0800

    Set character limits for name and phone number fields

--- src/main/java/seedu/address/model/person/Name.java
+++ src/main/java/seedu/address/model/person/Name.java
@@ -10,13 +10,13 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 public record Name(String fullName) {
 
     public static final String MESSAGE_CONSTRAINTS =
-            "Names should only contain alphanumeric characters and spaces, and it should not be blank";
+            "Names should only contain letters and spaces, and it should be 1-50 characters long.";
 
     /*
      * The first character of the address must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
-    public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
+    public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]{0,49}";
 
     /**
      * Constructs a {@code Name}.
