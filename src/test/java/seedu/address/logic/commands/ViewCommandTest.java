61447f8440dd4555850b07de9a5010b3201df7dd
Danny Chan <nihility.das@gmail.com>
2025-10-30 00:16:55 +0800

    Update to resolve code coverage

--- src/test/java/seedu/address/logic/commands/ViewCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ViewCommandTest.java
@@ -75,6 +75,16 @@ public class ViewCommandTest {
     }
 
     @Test
+    public void execute_partialKeywords_multiplePersonsFound() {
+        String expectedMessage = String.format(MESSAGE_PERSONS_LISTED_OVERVIEW, 1);
+        NameContainsKeywordsPredicate predicate = preparePredicate("Ell");
+        ViewCommand command = new ViewCommand(predicate);
+        expectedModel.updateFilteredPersonList(predicate);
+        assertCommandSuccess(command, model, expectedMessage, expectedModel);
+        assertEquals(Arrays.asList(ELLE), model.getFilteredPersonList());
+    }
+
+    @Test
     public void toStringMethod() {
         NameContainsKeywordsPredicate predicate = new NameContainsKeywordsPredicate(Arrays.asList("keyword"));
         ViewCommand viewCommand = new ViewCommand(predicate);
