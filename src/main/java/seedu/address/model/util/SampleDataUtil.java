755945c2edc547f49e15bdbc98fed1f8b600294f
Caleb Goh <calebgoh05@gmail.com>
2025-10-29 11:22:42 +0800

    Add unit tests for JsonAdaptedPerson

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -1,6 +1,7 @@
 package seedu.address.model.util;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -12,6 +13,7 @@ import seedu.address.model.person.GitHubUsername;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
+import seedu.address.model.person.Salary;
 import seedu.address.model.tag.Tag;
 
 /**
@@ -21,27 +23,23 @@ public class SampleDataUtil {
     public static Person[] getSamplePersons() {
         return new Person[] {
             new Person("E1001", new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
-                new Address("Blk 30 Geylang Street 29, #06-40"), new GitHubUsername("@alexxx"),
-                getTagSet("friends")),
+                    new Address("Blk 30 Geylang Street 29, #06-40"), new GitHubUsername("@alexxx"),
+                    new HashSet<>(), getTagSet("friends"), new Salary(1000.00)),
             new Person("E1002", new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
-                new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"), new GitHubUsername(
-                        "@bernyuuu"),
-                getTagSet("colleagues", "friends")),
+                    new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"), new GitHubUsername("@bernyuuu"),
+                    new HashSet<>(), getTagSet("colleagues", "friends"), new Salary(2000.00)),
             new Person("E1003", new Name("Charlotte Oliveiro"), new Phone("93210283"),
-                new Email("charlotte@example.com"), new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
-                    new GitHubUsername("@charolive3"),
-                getTagSet("neighbours")),
+                    new Email("charlotte@example.com"), new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
+                    new GitHubUsername("@charolive3"), new HashSet<>(), getTagSet("neighbours"), new Salary(3000.00)),
             new Person("E1004", new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
-                new Address("Blk 436 Serangoon Gardens Street 26, #16-43"), new GitHubUsername(
-                        "@lidavid01"),
-                getTagSet("family")),
+                    new Address("Blk 436 Serangoon Gardens Street 26, #16-43"), new GitHubUsername("@lidavid01"),
+                    new HashSet<>(), getTagSet("family"), new Salary(4000.00)),
             new Person("E1005", new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
-                new Address("Blk 47 Tampines Street 20, #17-35"), new GitHubUsername(
-                        "@irfanibrahim23"),
-                getTagSet("classmates")),
+                    new Address("Blk 47 Tampines Street 20, #17-35"), new GitHubUsername("@irfanibrahim23"),
+                    new HashSet<>(), getTagSet("classmates"), new Salary(5000.00)),
             new Person("E1006", new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
-                new Address("Blk 45 Aljunied Street 85, #11-31"), new GitHubUsername("@royb"),
-                getTagSet("colleagues"))
+                    new Address("Blk 45 Aljunied Street 85, #11-31"), new GitHubUsername("@royb"),
+                    new HashSet<>(), getTagSet("colleagues"), new Salary(6000.00))
         };
     }
 
@@ -69,5 +67,4 @@ public class SampleDataUtil {
                 .map(Tag::new)
                 .collect(Collectors.toSet());
     }
-
 }
--- src/test/java/seedu/address/storage/JsonAdaptedPersonTest.java
+++ src/test/java/seedu/address/storage/JsonAdaptedPersonTest.java
@@ -6,6 +6,7 @@ import static seedu.address.testutil.Assert.assertThrows;
 import static seedu.address.testutil.TypicalPersons.BENSON;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,8 +15,11 @@ import org.junit.jupiter.api.Test;
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
+import seedu.address.model.person.GitHubUsername;
 import seedu.address.model.person.Name;
+import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
+import seedu.address.model.person.Salary;
 
 public class JsonAdaptedPersonTest {
     private static final String INVALID_NAME = "R@chel";
@@ -23,6 +27,7 @@ public class JsonAdaptedPersonTest {
     private static final String INVALID_ADDRESS = " ";
     private static final String INVALID_EMAIL = "example.com";
     private static final String INVALID_GITHUBUSERNAME = "-johnd*oe";
+    private static final String INVALID_SALARY = "fifty thousand";
     private static final String INVALID_TAG = "#friend";
 
     private static final String VALID_ID = BENSON.id();
@@ -31,7 +36,7 @@ public class JsonAdaptedPersonTest {
     private static final String VALID_EMAIL = BENSON.email().value();
     private static final String VALID_ADDRESS = BENSON.address().value();
     private static final String VALID_GITHUBUSERNAME = BENSON.gitHubUsername().value();
-    private static final String VALID_SALARY = String.valueOf(BENSON.salary());
+    private static final String VALID_SALARY = String.valueOf(BENSON.salary().toDouble());
     private static final List<String> VALID_TEAMIDS = BENSON.teamIds().stream()
                                                         .toList();
     private static final List<JsonAdaptedTag> VALID_TAGS = BENSON.tags().stream()
@@ -125,4 +130,47 @@ public class JsonAdaptedPersonTest {
                         VALID_ADDRESS, VALID_GITHUBUSERNAME, VALID_SALARY, VALID_TEAMIDS, invalidTags);
         assertThrows(IllegalValueException.class, person::toModelType);
     }
+
+    @Test
+    public void toModelType_invalidGitHubUsername_throwsIllegalValueException() {
+        JsonAdaptedPerson person =
+                new JsonAdaptedPerson(VALID_ID, VALID_NAME, VALID_PHONE, VALID_EMAIL,
+                        VALID_ADDRESS, INVALID_GITHUBUSERNAME, VALID_SALARY, VALID_TEAMIDS, VALID_TAGS);
+        String expectedMessage = GitHubUsername.MESSAGE_CONSTRAINTS;
+        assertThrows(IllegalValueException.class, expectedMessage, person::toModelType);
+    }
+
+    @Test
+    public void toModelType_nullGitHubUsername_throwsIllegalValueException() {
+        JsonAdaptedPerson person = new JsonAdaptedPerson(
+                VALID_ID, VALID_NAME, VALID_PHONE, VALID_EMAIL, VALID_ADDRESS, null, VALID_SALARY,
+                VALID_TEAMIDS, VALID_TAGS);
+        String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, GitHubUsername.class.getSimpleName());
+        assertThrows(IllegalValueException.class, expectedMessage, person::toModelType);
+    }
+
+    @Test
+    public void toModelType_nullSalary_throwsIllegalValueException() {
+        JsonAdaptedPerson person = new JsonAdaptedPerson(VALID_ID, VALID_NAME, VALID_PHONE, VALID_EMAIL,
+                VALID_ADDRESS, VALID_GITHUBUSERNAME, null, VALID_TEAMIDS, VALID_TAGS);
+        String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, Salary.class.getSimpleName());
+        assertThrows(IllegalValueException.class, expectedMessage, person::toModelType);
+    }
+
+    @Test
+    public void toModelType_invalidSalary_throwsNumberFormatException() {
+        JsonAdaptedPerson person = new JsonAdaptedPerson(VALID_ID, VALID_NAME, VALID_PHONE, VALID_EMAIL,
+                VALID_ADDRESS, VALID_GITHUBUSERNAME, INVALID_SALARY, VALID_TEAMIDS, VALID_TAGS);
+        assertThrows(NumberFormatException.class, person::toModelType);
+    }
+
+    @Test
+    public void toModelType_nullTeamIds_createsPersonWithEmptyTeamIdSet() throws Exception {
+        JsonAdaptedPerson person = new JsonAdaptedPerson(VALID_ID, VALID_NAME, VALID_PHONE, VALID_EMAIL,
+                VALID_ADDRESS, VALID_GITHUBUSERNAME, VALID_SALARY, null, VALID_TAGS);
+        Person expectedPerson = new Person(BENSON.id(), BENSON.name(), BENSON.phone(), BENSON.email(),
+                BENSON.address(), BENSON.gitHubUsername(), new HashSet<>(), BENSON.tags(), BENSON.salary());
+        assertEquals(expectedPerson, person.toModelType());
+    }
+
 }
