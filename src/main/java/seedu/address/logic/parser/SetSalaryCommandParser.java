2100b926ce686aa61e9015c1eec8761996373f71
Z-Puyu <zpyzhangpuyu@gamil.com>
2025-10-30 00:17:08 +0800

    Remove unused imports

--- src/main/java/seedu/address/logic/parser/SetSalaryCommandParser.java
+++ src/main/java/seedu/address/logic/parser/SetSalaryCommandParser.java
@@ -2,8 +2,6 @@ package seedu.address.logic.parser;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 import seedu.address.logic.Messages;
